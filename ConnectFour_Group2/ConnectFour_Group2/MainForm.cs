4c94ee712854e00f450ba8be65b0706cb581a79f
Todd Coleman <coleman_todd_2@rocketmail.com>
2025-04-13 00:42:23 -0400

    made the buttons of the first form work
    i finished out the skeletons i added earlier and made the reading from the file happen, as well as made a player class

--- ConnectFour_Group2/ConnectFour_Group2/MainForm.cs
+++ ConnectFour_Group2/ConnectFour_Group2/MainForm.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,29 +24,88 @@ namespace ConnectFour_Group2
         //this will be the welcome form
 
 
+
+        List<Player> players = new List<Player>();
         public MainForm()
         {
             InitializeComponent();
+            addFromFile();
+            //putting a table here for reference
+            //0 = player 1, 1 = player 2, 2 = computer
+            //Console.WriteLine(players[0].getPlay());
+            //Console.WriteLine(players[1].getPlay());
+            //Console.WriteLine(players[2].getPlay());
+            ////=============================================================
+            ////this is testing to make sure it is stored properly
+            ////also for if you guys ever need to check when doing something
+            ////=============================================================
+            //Console.WriteLine(players[0].getScore());
+            //Console.WriteLine(players[1].getScore());
+            //Console.WriteLine(players[2].getScore());
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            BoardForm formtoload = new BoardForm();
+            loadNewForm(formtoload);
+            this.Hide();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            BoardForm formtoload = new BoardForm();
+            loadNewForm(formtoload);
+            this.Hide();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            StatsForm formtoload = new StatsForm();
+            loadNewForm(formtoload);
+            this.Hide();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+
+
+        public void loadNewForm(Form ftl)
+        {
+            ftl.Show();
+            ftl.BringToFront();
+        }
+
+        public void addFromFile()
+        {
+            StreamReader file = new StreamReader("../../Scores.txt");
+            string line = file.ReadLine();
+
+            string player;
+            string score;
+
+            int comma;
+            char delim = ',';
+
+            Player newPlayer;
+
+            while (line != null)
+            {
+                comma = line.IndexOf(delim);
+                player = line.Substring(0, comma);
+                line = line.Substring(comma + 1);
+                comma = line.IndexOf(delim);
+                score = line;
+
+                newPlayer = new Player(player, score);
+                players.Add(newPlayer);
+
+                line = file.ReadLine();
+            }
+            file.Close();
+        }
+
     }
 }
---
+++ ConnectFour_Group2/ConnectFour_Group2/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour_Group2
+{
+    internal class Player
+    {
+        private string play;
+        private string score;
+
+        public Player(string p, string s)
+        {
+            play = p;
+            score = s;
+        }
+        //=======================Getters=======================
+        public string getPlay()
+        {
+            return play;
+        }
+        public string getScore()
+        {
+            return score;
+        }
+        //=======================Setters=======================
+        public void setPlay(string p)
+        {
+            play = p;
+        }
+        public void setScore(string s)
+        {
+            score = s;
+        }
+    }
+}
